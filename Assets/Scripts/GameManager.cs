d8a86fadff9b218f4bf52c20fe0463c84a10429a
Nour-Hossam28 <nour.farahat@student.giu-uni.de>
2026-05-07 13:39:46 +0300

    added diary pickup scripts and assets for Room 2

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public int diariesCollected = 0;
+    public TextMeshProUGUI progressText;
+    public Image progressBarFill;
+    public TextMeshProUGUI completionMessage;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public void CollectDiary()
+    {
+        diariesCollected++;
+
+        float fillAmount = diariesCollected / 3f;
+        progressBarFill.fillAmount = fillAmount;
+        progressText.text = "Diaries Found: " 
+                          + diariesCollected + " / 3";
+
+        if (diariesCollected >= 3)
+        {
+            StartCoroutine(ShowMessageAndTransition());
+        }
+    }
+
+    IEnumerator ShowMessageAndTransition()
+    {
+        completionMessage.gameObject.SetActive(true);
+        completionMessage.text = 
+            "You have found all the diaries...\nThe truth awaits.";
+        yield return new WaitForSeconds(3f);
+        // transition to Room 3 will be added later
+    }
+}
